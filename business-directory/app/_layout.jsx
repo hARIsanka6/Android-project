ca840f351890125a869345fc5f98c942cae263ae
Harisankar <harisankarlflower@gmail.com>
2025-02-12 10:35:39 +0530

    first commit

---
+++ business-directory/app/_layout.jsx
@@ -0,0 +1,31 @@
+import { useFonts } from "expo-font";
+import { Stack } from "expo-router";
+import { ClerkProvider, SignedIn, SignedOut} from '@clerk/clerk-expo'
+import { Text, View } from "react-native";
+import LoginScreen from "../app-example/components/loginScreen"
+
+
+
+export default function RootLayout() {
+
+  useFonts({
+    'Outfit': require('./../assets/fonts/Outfit-Bold.ttf')
+  })
+  return (
+    <ClerkProvider  publishableKey={process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY}>
+      <SignedIn>
+        <Stack screenOptions={{ headerShown:false}}>
+        <Stack.Screen name="(tabs)"/>
+        </Stack>
+        
+      </SignedIn>
+      <SignedOut>
+        <LoginScreen></LoginScreen>
+      </SignedOut>
+      
+    </ClerkProvider>
+    
+     
+    
+  );
+}
